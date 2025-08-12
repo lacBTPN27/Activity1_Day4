87fea21fd1c902ee60e32b533d35db0b03c40934
lacBTPN27 <lacvn2468@gmail.com>
2025-08-12 09:16:56 +0700

    tao tên lac

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 "Hello World! This is a simple Node.js application.";
 "Không biết nói gì hơn ngoài việc chào mừng bạn đến với ứng dụng này!";
+"Tao không biết nói gì hơn ngoài việc chào mừng bạn đến với ứng dụng này!";
+"Hello World! This is a simple Node.js application.";

796e3e2cc2e49224af56e09abc7918bed20781a6
lacBTPN27 <lacvn2468@gmail.com>
2025-08-12 09:19:25 +0700

    Revert "tao ten la gi"
    This reverts commit 87fea21fd1c902ee60e32b533d35db0b03c40934.

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 "Hello World! This is a simple Node.js application.";
 "Không biết nói gì hơn ngoài việc chào mừng bạn đến với ứng dụng này!";
-"Tao không biết nói gì hơn ngoài việc chào mừng bạn đến với ứng dụng này!";
-"Hello World! This is a simple Node.js application.";
